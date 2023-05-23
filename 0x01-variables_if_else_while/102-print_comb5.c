b7c3698e18be0c02754ad2b7eeb1bb4273fffa03
Giftbatolu <sekinatoyelami@gmail.com>
2023-05-22 19:07:42 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int  n, m;
+
+	for (n = 0; n <= 98; n++)
+	{
+		for (m = n + 1; m <= 99; m++)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((m / 10) + '0');
+			putchar((m / 10) + '0');
+			if (n == 98 && m  == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
