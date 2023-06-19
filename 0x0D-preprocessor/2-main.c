d02b712662adc01f079a01975145afc17167ccb1
Giftbatolu <sekinatoyelami@gmail.com>
2023-06-19 16:07:13 -0700

    2-main.c added

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that print the name of file
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("File name is:%s\n", __FILE__);
+	return (0);
+}
