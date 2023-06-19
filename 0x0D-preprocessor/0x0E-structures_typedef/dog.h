fa7a285ff2f8cd0cee646954c7eb607f13882e01
Giftbatolu <sekinatoyelami@gmail.com>
2023-06-19 16:45:10 -0700

    dog.h

---
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data type of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This is about dog and dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
