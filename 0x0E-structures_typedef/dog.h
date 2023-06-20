7c6f534df86254cd45a4b76e6bb00c2e03c46c64
Giftbatolu <sekinatoyelami@gmail.com>
2023-06-19 18:40:45 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,13 @@
  *
  * Description: This is a struct about dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+/* Further prototype function */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
