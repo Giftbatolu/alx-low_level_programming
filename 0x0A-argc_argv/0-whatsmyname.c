8a2a44eff096985b171c9366cef97a5aeca1a377
Giftbatolu <sekinatoyelami@gmail.com>
2023-10-07 14:14:02 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - The entry point
+ * @argc: The argument count.
+ * @argv: The argument vector.
+ *
+ * Return: always 0.
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a repository about argument pass to main function
