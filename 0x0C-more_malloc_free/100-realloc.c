be6760d5d9fd4ea501da4b568ed986cb6a9248dd
Giftbatolu <sekinatoyelami@gmail.com>
2023-10-08 15:04:37 -0700

    1

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _realloc - function that reallocate memory usin malloc and free.
+ * @ptr: The previous pointer allocated to the memory block.
+ * @old_size: The size allocated to the prevoius memory.
+ * @new_size: The new size allocte to the memory.
+ *
+ * Return: pointer to the allocated memory.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr;
+	/*char *old_ptr;*/
+	unsigned int b;
+
+	if (new_size == old_size)
+	return (ptr);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		return (malloc(new_size));
+	new_ptr = malloc(new_size);
+	if (new_ptr)
+	/*old_ptr = ptr;*/
+	{
+		for (b = 0; b < check(old_size, new_size); b++)
+		new_ptr[b] = ptr[b];
+		free(ptr);
+		return (new_size);
+	}
+	else
+		return (NULL);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,8 @@
 #include <stddef.h>
 #include <string.h>
 
+#define check(x, y) (((x) < (y)) ? (x) : (y))
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
