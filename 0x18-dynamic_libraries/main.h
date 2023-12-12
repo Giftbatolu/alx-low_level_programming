4a9969dd4d0601069a4a7a16e1e3193e2bf66a63
Giftbatolu <sekinatoyelami@gmail.com>
2023-12-11 18:29:38 -0800

    lib

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -22,4 +22,11 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+/* prototype of function in 100-operations.c*/
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
 #endif
